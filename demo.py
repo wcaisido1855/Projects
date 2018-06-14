60a5514dce1995001d4783fd5a6fc9e04b6db356
wcaisido1855 <31966519+wcaisido1855@users.noreply.github.com>
2018-06-14 12:49:01 -0700

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,14 @@
+from sklearn import tree
+
+# [height, weight, show size]
+X = [181,80,44], [177,70,43], [160,60,38], [154,54,37], [166,65,40], [190,90,47], [175,64,39], [177,70,40], [159,55,37], [171,75,42], [181,85,43]
+
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([200,90,63])
+
+print (prediction)
---
+++ demo2.py
@@ -0,0 +1,29 @@
+import tweepy
+from textblob import TextBlob
+
+# Step 1 - Authenticate
+consumer_key= 'XI8ezizHorsaJLvhR6CpRjBF1'
+consumer_secret= 'g9i6PDHqa7VhnBiK58zFuh9ENbVnliob7cqN1JfYAsBG7AyCgS'
+
+access_token='1688826414-eHrrNLNWH52Ct5TSmK2Wnwxaj2gTh8y49opH6wc'
+access_token_secret='78xDrsJnZhIdrDYFUW0tYaiNKqvg8aaNmfPk9f8QjC27Y'
+
+auth = tweepy. OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_token_secret)
+
+api = tweepy.API(auth)
+
+#Step 3 - Retrieve Tweets
+public_tweets = api.search('Trump')
+
+#CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
+#and label each one as either 'positive' or 'negative', depending on the sentiment 
+#You can decide the sentiment polarity threshold yourself
+
+
+for tweet in public_tweets:
+    print(tweet.text)
+    
+    #Step 4 Perform Sentiment Analysis on Tweets
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
