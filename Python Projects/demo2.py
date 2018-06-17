fd8a27744e906ab75f5f7d19d885efc7f738fc57
wcaisido1855 <31966519+wcaisido1855@users.noreply.github.com>
2018-06-17 11:50:01 -0700

    Update and rename demo2.py to Python Projects/demo2.py

--- Python Projects/demo2.py
+++ Python Projects/demo2.py
@@ -14,7 +14,7 @@ auth.set_access_token(access_token_token_secret)
 api = tweepy.API(auth)
 
 #Step 3 - Retrieve Tweets
-public_tweets = api.search('Trump')
+public_tweets = api.search('Kanye')
 
 #CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
 #and label each one as either 'positive' or 'negative', depending on the sentiment 
